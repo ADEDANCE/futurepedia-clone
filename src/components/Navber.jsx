8fde3741985c94974420aed6137afd9b7a2410a5
Adedance <uthmanadedance@gmail.com>
2026-03-06 12:48:53 +0100

    feat:Refractor change the nav color to blue

--- src/components/Navber.jsx
+++ src/components/Navber.jsx
@@ -1,34 +1,53 @@
 import React, { useState } from "react";
-
+import logo from "../assets/logo.svg";
+import { FiMenu } from "react-icons/fi";
+import { FiSearch } from "react-icons/fi";
 const Navber = () => {
   const [isOpen, setIsOpen] = useState(false);
   return (
-    <nav className=" bg-white shadow-md">
+    <nav className=" bg-blue-700 shadow-md">
       <div className=" max-w-7xl mx-auto px-4">
         <div className=" flex justify-between items-center h-16">
-          <h1 className="text-xl font-bold text-blue-600">MyWebsite</h1>
+          <img src={logo} alt="MyWebsite Logo" />
+
           <div className=" hidden md:flex space-x-6">
-            <a href="#" className="text-gray-700 hover:text-blue-600">
-              Home
+            <a
+              href="#"
+              className="text-white hover:text-gray-200 hover:underline"
+            >
+              AI Tools
             </a>
 
-            <a href="#" className="text-gray-700 hover:text-blue-600">
-              About
+            <a
+              href="#"
+              className="text-white hover:text-gray-200 hover:underline"
+            >
+              Newsletter
             </a>
 
-            <a href="#" className="text-gray-700 hover:text-blue-600">
-              Services
+            <a
+              href="#"
+              className="text-white hover:text-gray-200 hover:underline"
+            >
+              Resources
             </a>
-
-            <a href="#" className="text-gray-700 hover:text-blue-600">
-              Contact
-            </a>
+          </div>
+          <div className=" hidden md:flex space-x-6">
+            <button className="text-white text-xl">
+              <FiSearch />
+            </button>
+            <button className=" text-white hover:bg-white hover:text-black py-2 px-3 rounded-full">
+              Login
+            </button>
+            <button className=" bg-white text-black hover:bg-gray-300 py-2 px-3 rounded-full">
+              Join For Free
+            </button>
           </div>
           <button
             onClick={() => setIsOpen(!isOpen)}
-            className=" md:hidden text-gray-700"
+            className=" md:hidden text-white"
           >
-            ☰
+            <FiMenu className=" text-white" />
           </button>
         </div>
       </div>
