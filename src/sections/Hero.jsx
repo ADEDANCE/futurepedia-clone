ad433690a89a1a8eeaa7e9aad0bfde056e1c81ef
Adedance <uthmanadedance@gmail.com>
2026-03-07 15:48:21 +0100

    Refractor:change title text size

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,19 +1,29 @@
 import React from "react";
+import Button from "../components/Button";
 
 const Hero = () => {
   return (
-    <section className=" bg-purple-400 py-20">
+    <section className=" bg-blue-400 py-20">
       <div className=" max-w-7xl mx-auto px-4 text-center">
-        <h1 className="text-4xl md:text-5xl font-bold text-white">
-          Build Modern Websites With React
+        <h3 className=" text-xl font-bold text-white mb-8">
+          Join 350,000+ AI Adopters
+        </h3>
+        <h1 className="text-4xl md:text-7xl font-bold text-white">
+          Everything your business needs to master AI, all in one place.
         </h1>
-        <p className="mt-6 text-lg text-gray-300">
-          Learn how to build fast, responsive and modern websites using React
-          and Tailwind CSS.
-        </p>
-        <button className=" mt-8 px-6 py-3 bg-blue-600 text-white  rounded-lg hover:bg-blue-800">
-          Get Started
-        </button>
+        <h2 className="mt-6 text-2xl text-gray-100">
+          Explore top AI tools and learn how to use them effectively.
+        </h2>
+        <div className=" flex justify-center  gap-6 py-14">
+          <Button className={`bg-white text-black  hover:bg-gray-300`}>
+            Join For Free
+          </Button>
+          <Button
+            className={`text-white hover:text-black hover:bg-white border border-white `}
+          >
+            Get Started
+          </Button>
+        </div>
       </div>
     </section>
   );
