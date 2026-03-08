4db2e2757794668a80bd0c2b75df990f4bb00e7e
Adedance <uthmanadedance@gmail.com>
2026-03-08 15:26:26 +0100

    feat:import the ruusable card

--- src/sections/Popular_categories.jsx
+++ src/sections/Popular_categories.jsx
@@ -1,7 +1,37 @@
-import React from "react";
-import { FaStar, FaTrademark } from "react-icons/fa";
+import React, { useState } from "react";
+import {
+  FaCogs,
+  FaNetworkWired,
+  FaSellsy,
+  FaStar,
+  FaTag,
+  FaTrademark,
+} from "react-icons/fa";
 import KevinHutsonface from "../assets/KevinHutsonface.webp";
+import {
+  RiArrowDropUpLine,
+  RiCustomerService2Line,
+  RiHomeOfficeLine,
+} from "react-icons/ri";
+import { CiCalculator2 } from "react-icons/ci";
+import { GiNetworkBars } from "react-icons/gi";
+import { FaRegPenToSquare } from "react-icons/fa6";
+import { GrTechnology } from "react-icons/gr";
+import Home_card from "../components/Home_card";
+
 const Popular_categories = () => {
+  const [activeTab, setActiveTab] = useState("popular");
+  const icons = [
+    RiCustomerService2Line,
+    FaSellsy,
+    RiHomeOfficeLine,
+    CiCalculator2,
+    GiNetworkBars,
+    FaRegPenToSquare,
+    GrTechnology,
+    FaNetworkWired,
+    FaCogs,
+  ];
   const categories = [
     "Customer Service & Support",
     "Sales",
@@ -16,61 +46,120 @@ const Popular_categories = () => {
   return (
     <section className=" py-20">
       <div className=" max-w-7xl mx-auto px-4">
-        <div className=" flex gap-16 justify-center mb-10">
-          <div className=" flex gap-4 items-center">
-            <FaTrademark className=" text-gray-500 text-lg" />
-            <p className=" text-gray-500 text-lg">Popular Tools</p>
+        <div className=" mb-10">
+          <div className=" flex gap-16 justify-center">
+            <div
+              onClick={() => setActiveTab("popular")}
+              className={`flex gap-4 items-center  pb-3 cursor-pointer ${
+                activeTab === "popular" ? "text-blue-500" : "text-gray-500"
+              }`}
+            >
+              <FaTrademark className="text-lg" />
+              <p className=" text-lg">Popular Tools</p>
+            </div>
+            <div
+              onClick={() => setActiveTab("Recently")}
+              className={`flex gap-4 items-center pb-3 cursor-pointer ${
+                activeTab === "Recently" ? "text-blue-500" : "text-gray-500"
+              }`}
+            >
+              <FaStar className="  text-lg" />
+              <p className=" text-lg">Recently Added</p>
+            </div>
           </div>
-          <div className=" flex gap-4 items-center ">
-            <FaStar className=" text-gray-500 text-lg" />
-            <p className=" text-gray-500 text-lg">Popular Tools</p>
+
+          {/* Line */}
+          <div className="relative  h-0.5 w-96 bg-gray-300 mt-3 mx-auto">
+            <div
+              className={`absolute top-0 h-full  bg-blue-500 transition-all duration-300 ${
+                activeTab === "popular" ? "left-0 w-1/2 " : "left-1/2 w-1/2"
+              }`}
+            />
           </div>
         </div>
+
         <div className=" grid grid-cols-1 md:grid-cols-3 lg:gap-9">
-          <div>
+          <div className=" hidden md:block">
             <h2 className=" text-3xl font-bold text-black mb-4">
               Most Popular Categories
             </h2>
-            {categories.map((category, index) => (
-              <p className="text-gray-500 text-lg hover:pl-5 mb-4">
-                {" "}
-                {category}
-              </p>
-            ))}
+            {categories.map((category, index) => {
+              const Icon = icons[index]; // pick corresponding icon
+              return (
+                <div
+                  key={index}
+                  className=" flex gap-4 hover:pl-5 text-gray-500 text-lg  mb-4"
+                >
+                  <Icon />
+                  <p>{category}</p>
+                </div>
+              );
+            })}
           </div>
           <div className=" md:col-span-2">
             {/* card */}
-            <div className=" bg-white py-5 px-4 rounded-3xl">
-              <div className=" flex justify-between">
-                <div className=" flex gap-3">
-                  <img
-                    src={KevinHutsonface}
-                    alt=""
-                    className=" w-16 h-16 rounded-3xl"
-                  />
-                  <div>
-                    <h2 className=" text-3xl font-bold text-black ">ChatGPT</h2>
-                    <p className="text-gray-500 text-sm">
-                      Research, create, and automate tasks with the leader in
-                      AI.
-                    </p>
-                  </div>
-                </div>
-                <div className=" flex gap-3">
-                  <img
-                    src={KevinHutsonface}
-                    alt=""
-                    className=" w-16 h-16 rounded-3xl"
-                  />
-                  <img
-                    src={KevinHutsonface}
-                    alt=""
-                    className=" w-16 h-16 rounded-3xl"
-                  />
-                </div>
-              </div>
-              <div className=" bg-gray-300 h-0.5 w-full mt-3"></div>
-            </div>
+            <Home_card
+              image={KevinHutsonface}
+              title={"ChatGPT"}
+              description="Research, create, and automate tasks with the leader in AI."
+              stats={[
+                { icon: FaTag, value: 6215 },
+                {
+                  icon: RiArrowDropUpLine,
+                  value: 6215,
+                  color: "text-green-400",
+                },
+              ]}
+              tags={["#ai Chatbots", "#research", "#AI agents"]}
+            />
+
+            {/* card */}
+            <Home_card
+              image={KevinHutsonface}
+              title={"ChatGPT"}
+              description="Research, create, and automate tasks with the leader in AI."
+              stats={[
+                { icon: FaTag, value: 6215 },
+                {
+                  icon: RiArrowDropUpLine,
+                  value: 6215,
+                  color: "text-green-400",
+                },
+              ]}
+              tags={["#ai Chatbots", "#research", "#AI agents"]}
+            />
+
+            {/* card */}
+            <Home_card
+              image={KevinHutsonface}
+              title={"ChatGPT"}
+              description="Research, create, and automate tasks with the leader in AI."
+              stats={[
+                { icon: FaTag, value: 6215 },
+                {
+                  icon: RiArrowDropUpLine,
+                  value: 6215,
+                  color: "text-green-400",
+                },
+              ]}
+              tags={["#ai Chatbots", "#research", "#AI agents"]}
+            />
+
+            {/* card */}
+            <Home_card
+              image={KevinHutsonface}
+              title={"ChatGPT"}
+              description="Research, create, and automate tasks with the leader in AI."
+              stats={[
+                { icon: FaTag, value: 6215 },
+                {
+                  icon: RiArrowDropUpLine,
+                  value: 6215,
+                  color: "text-green-400",
+                },
+              ]}
+              tags={["#ai Chatbots", "#research", "#AI agents"]}
+            />
           </div>
         </div>
       </div>
