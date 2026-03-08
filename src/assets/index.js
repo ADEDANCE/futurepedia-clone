025873717074cf53a67bb67b512b4736ae18631b
Adedance <uthmanadedance@gmail.com>
2026-03-08 17:22:03 +0100

    feat:Create index.js for easy image importation

---
+++ src/assets/index.js
@@ -0,0 +1,7 @@
+import ChatGPTImage from "../assets/ChatGPTImage.png";
+import Claude from "../assets/Claude.png";
+import Gemini from "../assets/Gemini.png";
+import Grok from "../assets/Grok.png";
+import Perplexity from "../assets/Perplexity.png";
+
+export { ChatGPTImage, Claude, Gemini, Grok, Perplexity };
