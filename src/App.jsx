43d2b02ed4a7f15434e29bdc0f7d940b4bc3a10e
Adedance <uthmanadedance@gmail.com>
2026-03-08 11:41:38 +0100

    fest(Import popular category section)

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,20 @@ import Hero from "./sections/Hero";
 import Trending from "./sections/trending";
 import Footer from "./sections/Footer";
 import For_creator from "./sections/For_creator";
+import Popular_categories from "./sections/popular_categories";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
+    <div className=" bg-gray-200 min-h-screen">
       <Navber />
       <Hero />
+      <Popular_categories />
       <Trending />
       <For_creator />
       <Footer />
-    </>
+    </div>
   );
 }
 
